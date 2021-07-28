01bbbf79abcd01c090dd7b50be5dc1bb7e283112
aedenthorn <aedenthorn1@gmail.com>
2021-07-28 15:23:17 -0400

    etc

---
+++ Potions/PotionData.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Potions
+{
+    public class PotionData
+    {
+        public string id;
+        public string nameEN;
+        public string nameCN;
+        public string nameRU;
+        public string template;
+        public float health;
+        public float mana;
+        public float stamina;
+        public float rage;
+        public int rarity;
+        public int cost;
+        public int skillPoints;
+        public int attributePoints;
+        public string hotKey;
+        public Texture2D icon;
+        public Texture2D texture;
+        public Texture2D emission;
+    }
+}
