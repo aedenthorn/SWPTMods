0137f6e94e94d8a479e193ba3038404bc01ecb9d
aedenthorn <aedenthorn1@gmail.com>
2021-11-17 07:33:13 -0500

    etc

--- Tattoos/BepInExPlugin.cs
+++ Tattoos/BepInExPlugin.cs
@@ -12,7 +12,7 @@ using UnityEngine.UI;
 
 namespace Tattoos
 {
-    [BepInPlugin("aedenthorn.Tattoos", "Tattoos", "0.7.2")]
+    [BepInPlugin("aedenthorn.Tattoos", "Tattoos", "0.8.0")]
     public partial class BepInExPlugin : BaseUnityPlugin
     {
         public static BepInExPlugin context;
